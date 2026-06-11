bab7085e583bc92b3e4deb0cb3d4ac59a3076cca
kdeensha3-sketch <kdeensha3@gmail.com>
2026-06-11 14:48:42 +0530

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,19 @@
+--- Number Guessing Game ---
+I am thinking of a number between 1 and 100.
+Enter your guess: 32
+Too low! Try a higher number.
+Enter your guess: 45
+Too low! Try a higher number.
+Enter your guess: 70
+Too high! Try a lower number.
+Enter your guess: 56
+Too low! Try a higher number.
+Enter your guess: 60
+Too high! Try a lower number.
+Enter your guess: 58
+Too low! Try a higher number.
+Enter your guess: 59
+🎉 Congratulations! You guessed the number in 7 attempts.
+
+
+** Process exited - Return Code: 0 **
